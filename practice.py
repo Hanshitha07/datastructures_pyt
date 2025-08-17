87224ddb5291ec8abd521412933e4efc353f0fb9
Hanshitha Killi <killihanshitha7@gmail.com>
2025-08-17 23:54:57 +0530

    ,,,

--- practice.py
+++ practice.py
@@ -1,24 +1,55 @@
+# # def outer():
+# #     x = "outer"
+
+# #     def inner():
+# #         nonlocal x
+# #         x = "inner"
+# #     inner()
+# #     print(x)
+
+# # outer()  # Output: inner
+
+# x = "global"
+
 # def outer():
-#     x = "outer"
+#     x = "enclosing"
 
 #     def inner():
-#         nonlocal x
-#         x = "inner"
+#         global x
+#         x = "local"
+#         print(x)
 #     inner()
-#     print(x)
 
-# outer()  # Output: inner
+# outer()
+# print(x)  # Output: local
 
-x = "global"
+# class Car:
+#     # attributes (data / variables)
+#     brand = "Tesla"
+#     color = "Red"
+    
+#     # method (function inside class)
+#     def drive(self):
+#         print("The car is moving!")
+# # Creating objects (instances of Car)
+# car1 = Car()
+# car2 = Car()
 
-def outer():
-    x = "enclosing"
+# print(car1.brand)   # Tesla
+# print(car2.color)   # Red
 
-    def inner():
-        global x
-        x = "local"
-        print(x)
-    inner()
+# car1.drive()        # The car is moving!
+# car2.drive()        # The car is moving!
 
-outer()
-print(x)  # Output: local
+class Dog:
+    # constructor
+    def __init__(self, name, breed):
+        self.name = name   # attribute
+        self.breed = breed # attribute
+
+# creating objects
+dog1 = Dog("Bruno", "Labrador")
+dog2 = Dog("Simba", "German Shepherd")
+
+print(f"{dog1.name},- {dog1.breed}")
+print(f"{dog2.name},- {dog2.breed}")
